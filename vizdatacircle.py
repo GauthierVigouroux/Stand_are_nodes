610fdd39b3acf65e1af0df1bc36e9489ab000ae4
Gauthier Vigouroux <gauthier.vigouroux@ecole-navale.fr>
2022-04-28 14:51:28 +0200

    holoviz

--- vizdatacircle.py
+++ vizdatacircle.py
@@ -1,5 +1,5 @@
+from cgitb import html
 import pandas as pd
-from pandas import json_normalize
 import numpy as np
 from jsonreading import readJsonFileViz
 
@@ -15,23 +15,62 @@ ISODico = readJsonFileViz("data.json")
 #df = json_normalize(ISODico)
 #print(df)
 ##Solution 2 (readjson)
-#links = pd.read_json("data.json", orient='index')
-#print(links.head())
+# links = pd.read_json("data.json", orient='index')
+# print(links.head())
+
+
+sourcelist = list()
+targetlist = list()
+valuelist = list()
+
+for key in ISODico.keys():
+    for iso in ISODico[key]["dependance"]:
+        if not(ISODico[key]["dependance"]) == False:
+            sourcelist.append(ISODico[key]["short"])
+            targetlist.append(ISODico[iso]["short"])
+            valuelist.append(ISODico[key]["dependance"].count(iso))
+source = pd.DataFrame(sourcelist)
+target = pd.DataFrame(targetlist)
+value = pd.DataFrame(valuelist)
+
+links = pd.concat([source, target, value], axis=1)
+links.columns = ['source', 'target', 'value']
+print(links.head())
+
+#Voici ma liste de normes
+ISOListlien =list()
+ISOListnom = list()
+for key in ISODico.keys():
+#    ISOListlien.append(key)
+    ISOListnom.append(ISODico[key]["short"])
+#ISOListlien = pd.DataFrame(ISOListlien)
+# ISOListnom = pd.DataFrame(ISOListnom)
+# ISODataFrame = pd.concat([ISOListnom],axis=1)
+ISODataSet = hv.Dataset(pd.DataFrame(ISOListnom,columns=["ISO"]))
+# Filtrage sur le nombre de référence minimum
+min_nav = 2
+hv.extension('bokeh')
+chord = hv.Chord((links, ISODataSet)).select(value=(min_nav, None))
+
 
 
 
 ############################################################################################
-#Voici ma liste de normes
-ISOList = []
-for key in ISODico.keys():
-    ISOList.append(ISODico[key]["short"])
 
+def rotate_label(plot, element):
+    white_space = "                  "
+    angles = plot.handles['text_1_source'].data['angle']
+    characters = np.array(plot.handles['text_1_source'].data['text'])
+    plot.handles['text_1_source'].data['text'] = np.array([x + white_space if x in characters[np.where((angles < -1.5707963267949) | (angles > 1.5707963267949))] else x for x in plot.handles['text_1_source'].data['text']])
+    plot.handles['text_1_source'].data['text'] = np.array([white_space + x if x in characters[np.where((angles > -1.5707963267949) | (angles < 1.5707963267949))] else x for x in plot.handles['text_1_source'].data['text']])
+    angles[np.where((angles < -1.5707963267949) | (angles > 1.5707963267949))] += 3.1415926535898
+    plot.handles['text_1_glyph'].text_align = "center"
 
 # linkslist = ISODico["https://www.iso.org/obp/ui/#iso:std:iso:19136:-2:en"]["dependance"]
 # # print(linkslist)
 # print(linkslist.count("https://www.iso.org/obp/ui/#iso:std:iso:19108:en"))
 
-# #Besoin d'un liste de couple url_src url_dest et d'un count pour le nombre de fois
+# #Comptage du nombre de liens présent pour notre column valeur 
 # coupleList = []
 # for key in ISODico.keys():
 #     for link in ISODico[key]["dependance"]:
@@ -40,16 +79,7 @@ for key in ISODico.keys():
 
 #Affichage du diagramme de dépendance
 # chord = hv.Chord((coupleList,ISOList))
-# chord.opts(
-#     opts.Chord(node_color="ISO", node_cmap="Category20", cmap='Category20', edge_cmap='Category20', 
-#                labels='ISO', edge_line_width=2, height=500, width=500, title="ISO 19100 series dependancies", hooks=[rotate_label]))
+chord.opts(node_color="ISO", node_cmap="Category20", cmap='Category20', edge_cmap='Category20', edge_color="source",
+               labels='ISO', edge_line_width=2, height=1500, width=1500, title="ISO 19100 series dependancies", hooks=[rotate_label])
 
-
-# def rotate_label(plot, element):
-#     white_space = "                  "
-#     angles = plot.handles['text_1_source'].data['angle']
-#     characters = np.array(plot.handles['text_1_source'].data['text'])
-#     plot.handles['text_1_source'].data['text'] = np.array([x + white_space if x in characters[np.where((angles < -1.5707963267949) | (angles > 1.5707963267949))] else x for x in plot.handles['text_1_source'].data['text']])
-#     plot.handles['text_1_source'].data['text'] = np.array([white_space + x if x in characters[np.where((angles > -1.5707963267949) | (angles < 1.5707963267949))] else x for x in plot.handles['text_1_source'].data['text']])
-#     angles[np.where((angles < -1.5707963267949) | (angles > 1.5707963267949))] += 3.1415926535898
-#     plot.handles['text_1_glyph'].text_align = "center"
+hv.save(chord, 'chordviz.html',fmt='html')
